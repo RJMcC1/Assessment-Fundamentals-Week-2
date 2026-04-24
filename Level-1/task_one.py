210a421126eae8e0e2c7a59fbfb4c917ffed734d
Ryan McCulloch <ryanmcculloch@Ryans-MacBook-Pro.local>
2026-04-24 17:15:43 +0100

    pylint task one

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,49 +1,47 @@
 from datetime import date
 
-
 class Assessment:
-    def __init__(self, name:str , type:str , score:float):        
+    '''Assessment parent class, validates types, claculates weight'''
+    def __init__(self, name:str , type:str , score:float):
         self.name = name
         self.type = type
         self.score = score
 
         if self.score > 100:
             raise ValueError("Can't be higher then 100")
-        
+
         if self.score < 0:
             raise ValueError("Can't be lower then 0")
-        
-        if self.type != "multiple-choice" : 
-            if self.type !="technical" :
+
+        if self.type != "multiple-choice":
+            if self.type !="technical":
                 if self.type != "presentation":
                     raise ValueError("invalid type")
-        
 
 
 class Trainee:
+    '''Trainee class, contains methods get_age, and variations of get_assessment'''
     def __init__(self, name:str , email:str , date_of_birth:date):
         self.name = name
         self.email = email
         self.date_of_birth = date_of_birth
         self.assessments = list()
-        
 
     def get_age(self):
+        '''Gets your age from today'''
         age = date.today() - self.date_of_birth
         return age.days // 365
-    
+
     def add_assessment(self, assessment: Assessment) -> None:
+        '''Checks if assessment is list and adds to empty list'''
         return self.assessments.append(assessment)
 
     def get_assessment(self ,name: str) -> Assessment | None:
+        '''Looks for name of assessments and return them if there'''
         for assessment in self.assessments:
             if assessment.name == name:
                 return assessment
             return None
-
-
-
-
 
 
 if __name__ == "__main__":
